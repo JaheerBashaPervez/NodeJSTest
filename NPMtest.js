1769024a355a018af84f3fdd089f7708478c1a8c
Jaheer <zaheermehmood7@gmail.com>
2022-01-25 01:02:38 -0500

    adding extra files

--- NPMtest.js
+++ NPMtest.js
@@ -1,3 +1,9 @@
 var calc = require("./functions.js");
 result = calc.add(4, 5);
 console.log("The output is " + result);
+
+nameDisplay("jaheer");
+
+function nameDisplay(name) {
+  console.log(name.);
+}
